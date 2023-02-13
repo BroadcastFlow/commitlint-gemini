3b97053bc54ae16d0446b3d90f738d100322ba4b
Jordan Hall <Jordan@lts.sh>
2023-02-13 17:01:11 +0000

    fix: get the rules working via npm link
    Signed-off-by: Jordan Hall <Jordan@lts.sh>

---
+++ packages/commitlint-config-gemini/src/index.ts
@@ -0,0 +1,7 @@
+export const commitlintConfigGemini = {
+  rules: {
+    'gemini-task-rule': [2, 'always'],
+  },
+}
+
+export default commitlintConfigGemini
--- packages/commitlint-gemini-task/src/index.ts
+++ packages/commitlint-gemini-task/src/index.ts
@@ -1,32 +1,13 @@
 import type {
-  BaseRule,
-  UserConfig,
-  Commit,
-  RuleConfigCondition,
-  Value
+  Plugin,
 } from '@commitlint/types';
+import { gemiTask } from './rules/geminiTaskRule';
 
 
-export const GemiTask: BaseRule<string, 'sync'> = (parsed: Commit, when?: RuleConfigCondition | undefined, value?: Value | undefined) => {
-  const taskIdRegex = /\b(GEM:[A-Z]{2,3}-[0-9]+)\b/;
-
-  if (!parsed.references.some(val => val.raw.match(taskIdRegex))) {
-    return [
-      when === 'always' ? value : false,
-      `Subject must include a task ID from Countersoft Gemini, prefixed with "GEM-", followed by 2-3 characters, a hyphen, and then numbers, such as GEM-INT-35707 or GEM-PL-35698`,
-    ];
-  }
-
-  return [true];
-};
-
-
-
-
-const config: UserConfig = {
+const config: Plugin = {
   rules: {
-    'gem-task': [2, 'always', GemiTask]
-  }
+    'gemini-task-rule': gemiTask
+  },
 }
 
 export default config;
