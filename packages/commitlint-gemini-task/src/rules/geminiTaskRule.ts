3668c8ee8a42baac9aa906781deaaf0995d31812
Jordan Hall <Jordan@lts.sh>
2023-02-15 10:04:20 +0000

    fix: change the regex for task ID
    Signed-off-by: Jordan Hall <Jordan@lts.sh>

--- packages/commitlint-gemini-task/src/rules/geminiTaskRule.ts
+++ packages/commitlint-gemini-task/src/rules/geminiTaskRule.ts
@@ -1,11 +1,12 @@
 import type { BaseRule } from '@commitlint/types';
 
 export const gemiTask: BaseRule<string, 'sync'> = (parsed, when, value) => {
-  const taskIdRegex = /\b(GEM:[A-Z]{2,3}-[0-9]+)\b/;
+  // const taskIdRegex = /\b(GEM:[A-Z]{2,3}-[0-9]+)\b/;
+  const taskIdRegex = /\b(GEM:=[0-9]+)\b/;
   if (!taskIdRegex.test(parsed.body ?? '')) {
     return [
       when === 'always' ? !!value : false,
-      `Subject must include a task ID from Countersoft Gemini, prefixed with "GEM:", followed by 2-3 characters, a hyphen, and then numbers, such as GEM:INT-35707 or GEM:PL-35698`,
+      `Subject must include a task ID from Countersoft Gemini, prefixed with "GEM:", followed by 2-3 characters, a hyphen, and then numbers, such as GEM:35707 or GEM:35698`,
     ];
   }
 
